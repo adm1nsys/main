3dac54cec08f97300d717c57b14fcc4bebbd3d4e
Admin inc <107643055+administrati0n@users.noreply.github.com>
2023-02-14 22:03:05 +0200

    Add files via upload

---
+++ all scripts/script3.js
@@ -0,0 +1,18 @@
+  $(document).ready(function() {
+  
+  setTimeout(function(){
+    $('body').addClass('loaded');
+    $('h1').css('color','#222222');
+    // var audio = new Audio(); 
+    // Создаём новый элемент Audio
+  // audio.src = 'LoadSound.mp3'; 
+  // Указываем путь к звуку
+  // audio.autoplay = true; 
+  // Автоматически запускаем
+var audio = new Audio();
+audio.preload = 'auto';
+audio.src = 'LoadSound.mp3';
+audio.play();
+  }, 16000);
+  
+});
